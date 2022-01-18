d8ee511e1b66caaad88fea9b4083927f0e284560
AleksandrTeterin21 <tet3.teterin@yandex.ru>
2022-01-18 15:33:21 +0500

    3

--- app/src/main/java/com/example/demo/SignInScreen.kt
+++ app/src/main/java/com/example/demo/SignInScreen.kt
@@ -3,20 +3,37 @@ package com.example.demo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class SignInScreen : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_screen)
-
+        email = findViewById(R.id.Email)
+        password = findViewById(R.id.Password)
     }
 
     fun reg(view: View) {
-        val intent = Intent(this@SignInScreen,MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        if(Patterns.EMAIL_ADDRESS.matcher(email.text).matches() && email.text.isNotEmpty()){
+            val intent = Intent(this@SignInScreen,MainActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Ошибка")
+                .setMessage("Неправильно заполнено поле почты")
+                .setPositiveButton("OK", null)
+                .create()
+                .show()
+        }
     }
+
+
     fun sign(view: View) {
         val intent = Intent(this@SignInScreen,SignUpScreen::class.java)
         startActivity(intent)
